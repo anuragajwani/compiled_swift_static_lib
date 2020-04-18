1e9d4adbee5b799c93f969d528fd69d5a0ba5602
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-04-18 11:05:28 +0100

    Added empty MyApp

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Anurag Ajwani on 18/04/2020.
+//  Copyright © 2020 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
